76fd3d2f825c1beefca0a771cfe58e1a49786e93
Shihab066 <md.shihab066@gmail.com>
2024-03-11 15:27:03 +0600

    made some change in firbase

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_apiKey,
-    authDomain: import.meta.env.VITE_authDomain,
-    projectId: import.meta.env.VITE_projectId,
-    storageBucket: import.meta.env.VITE_storageBucket,
-    messagingSenderId: import.meta.env.VITE_messagingSenderId,
-    appId: import.meta.env.VITE_appId
+    apiKey: 'AIzaSyAnkaQniuECp_H-EMNCLq5tsyf0LNr-t7Q',
+    authDomain: 'sea-properties-us.firebaseapp.com',
+    projectId: 'sea-properties-us',
+    storageBucket: 'sea-properties-us.appspot.com',
+    messagingSenderId: '715977298465',
+    appId: '1:715977298465:web:a9dd1339126d8b03c84960'
 };
 
 // Initialize Firebase
