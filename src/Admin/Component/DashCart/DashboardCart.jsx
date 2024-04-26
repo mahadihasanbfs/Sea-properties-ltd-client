dcb79c3f034a57d13a2d8baaf721d8c8ccdabbd7
sarwar-asik <sarwarasik@gmail.com>
2024-04-26 16:35:13 +0600

    admin/dashboard: total Order and Project done

--- src/Admin/Component/DashCart/DashboardCart.jsx
+++ src/Admin/Component/DashCart/DashboardCart.jsx
@@ -1,9 +1,13 @@
 import TotalUser from "./TotalUser";
+import TotalProjects from "./TotalProjects";
+import TotalOrders from "./TotalOrders";
 
 const DashboardCart = () => {
   return (
     <div className="grid md:grid-cols-3 lg:gap-4 md:gap-2 gap-1 mt-4">
       <TotalUser />
+      <TotalProjects />
+      <TotalOrders />
     </div>
   );
 };
