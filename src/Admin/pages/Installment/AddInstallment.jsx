b1ffa7c634f1381a53517e8fd02ad13bb3b3eaf0
sarwar-asik <sarwarasik@gmail.com>
2024-04-26 19:28:46 +0600

    admin/intallment: added react select in installment

--- src/Admin/pages/Installment/AddInstallment.jsx
+++ src/Admin/pages/Installment/AddInstallment.jsx
@@ -7,6 +7,7 @@ import Swal from "sweetalert2";
 import useFetchData from "../../../hooks/useFetchData";
 import { DB_URL } from "../../../const";
 import { useNavigate } from "react-router-dom";
+import Select from "react-select";
 
 const AddInstallment = () => {
   // const [value, setValue] = useState("");
@@ -24,7 +25,9 @@ const AddInstallment = () => {
 
   // Event handler for when an option is selected
   const handleInstallmentChange = (e) => {
-    setSelectedInstallmentUser(e.target.value);
+    console.log("e", e);
+
+    setSelectedInstallmentUser(e);
   };
 
   console.log(selectedInstallmentUser);
@@ -50,7 +53,7 @@ const AddInstallment = () => {
       amount,
       contact,
       installment,
-      email: selectedInstallmentUser,
+      email: selectedInstallmentUser?.value,
     };
     console.log(data);
 
@@ -85,22 +88,19 @@ const AddInstallment = () => {
         <div>
           <label htmlFor="Installment">Select Installment:</label>
           <br />
-          <select
+          <Select
             id="Installment"
             value={selectedInstallmentUser}
             onChange={handleInstallmentChange}
             required
-            className="rounded-lg w-full border border-[#336cb6] px-4 py-2 bg-[white] text-[#336cb6] ring-offset-2 duration-300 focus:outline-none focus:ring-2"
-          >
-            <option disabled selected>
-              Select User
-            </option>
-            {userData?.map((pkg) => (
-              <option className="text-black" key={pkg.email} value={pkg.email}>
-                {pkg.email}
-              </option>
-            ))}
-          </select>
+            className="rounded-lg w-full border border-[#336cb6]  bg-[white] text-[#336cb6] ring-offset-2 duration-300 focus:outline-none focus:ring-2"
+            options={userData
+              ?.filter((item) => item?.role !== "admin")
+              ?.map((user) => ({
+                label: user.email,
+                value: user.email,
+              }))}
+          />
         </div>
         <div className="mb-4">
           <label htmlFor="name">Name</label>
