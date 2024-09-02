8e453ebf7d50cd22c053e34c6d06e90553c0b620
Md. Mahadi Hasan <73072248+codewithmahadihasan@users.noreply.github.com>
2024-09-03 02:32:08 +0600

    update code

--- src/pages/Home/Contact/Contact.jsx
+++ src/pages/Home/Contact/Contact.jsx
@@ -11,166 +11,166 @@ import con2 from '../../../assets/con2.jpg';
 
 
 const Contact = () => {
-  const { ContactPageImg } = useContextApi();
-  const { bannerImg, contactImg } = ContactPageImg;
-  const [loading, setLoading] = useState(false);
+      const { ContactPageImg } = useContextApi();
+      const { bannerImg, contactImg } = ContactPageImg;
+      const [loading, setLoading] = useState(false);
 
-  const contactInfo = {
-    title: "Contact Us",
-    companyName: "SEA Properties Limited",
+      const contactInfo = {
+            title: "Contact Us",
+            companyName: "SEA Properties Limited",
 
-    phone: [
-      //every phone item create new line.
-      "Cell: +8801894-440111",
-      "Email: info@seapropertiesltd.com.bd",
-    ],
-  };
+            phone: [
+                  //every phone item create new line.
+                  "Cell: +8801894-440111",
+                  "Email: info@seapropertiesltd.com.bd",
+            ],
+      };
 
-  const { title, companyName, phone } = contactInfo;
+      const { title, companyName, phone } = contactInfo;
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const form = event.target;
-    const name = form.name.value;
-    const email = form.email.value;
-    const phone = form.phone.value;
-    const message = form.message.value;
+      const handleSubmit = (event) => {
+            event.preventDefault();
+            const form = event.target;
+            const name = form.name.value;
+            const email = form.email.value;
+            const phone = form.phone.value;
+            const message = form.message.value;
 
-    const data = {
-      name,
-      email,
-      phone,
-      message,
-      date: new Date(),
-    };
-    console.log(data);
+            const data = {
+                  name,
+                  email,
+                  phone,
+                  message,
+                  date: new Date(),
+            };
+            console.log(data);
 
-    fetch(`${DB_URL}/user/contact/add`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        setLoading(false);
-        form.reset();
-        console.log(data);
-        Swal.fire("Thanks for contacting us. We will get back to you shortly", "", "success");
-      });
-  };
-  return (
-    <div>
-      <Helmet>
-        <title>Contact | SEA Properties Ltd.</title>
-      </Helmet>
-      {/* Contact banner section */}
-      <PrimaryBanner
-        title={"Get in touch"}
-        subTitle={"Contact"}
-        bannerImg={ss}
-        opacity={30}
-      ></PrimaryBanner>
-
-      {/* Contact Info */}
-      <div className="grid md:grid-cols-2">
-        <figure>
-          <img src={con2} className="w-full h-[390px] object-cover" />
-        </figure>
-        <div className="space-y-6 pl-6 md:pl-[50px] lg:pl-[100px] my-auto py-6 md:py-0">
-          <SecondaryTitle text={title} position="text-left" />
-          <p className="text-[24px]">{companyName}</p>
-
-          <div>
-            <h1 className="md:text-[24px] text-[20px]"> Our Address:</h1>
-
-            <p className="md:text-[24px] md:block hidden text-[20px]" >
-              100 North Kalshi,
-              Gate No - 01,
-              Mirpur DOHS <br /> Dhaka, Bangladesh
-            </p>
-
-            <p className="md:text-[24px] md:hidden block text-[20px]" >
-              100 North Kalshi,
-              Gate No - 01,
-              Mirpur DOHS,  Dhaka, Bangladesh
-            </p>
-
-          </div>
-
-          <div>
-            {phone?.map((item, index) => (
-              <p className="md:text-[24px] text-[20px]" key={index}>{item}</p>
-            ))}
-          </div>
-        </div>
-      </div>
-
-      {/* Contact form */}
-      <div className="bg-[#768f9b] py-[80px] lg:py-[105px]">
-        <div className="max-w-[1366px] mx-auto px-6 md:px-[90px] lg:px-[260px] text-white">
-          <h2 className="text-[27px] md:text-[37px] uppercase">
-            Book A Free Appointment
-          </h2>
-          <form onSubmit={handleSubmit} className="space-y-4 mt-10">
+            fetch(`${DB_URL}/user/contact/add`, {
+                  method: "POST",
+                  headers: {
+                        "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify(data),
+            })
+                  .then((res) => res.json())
+                  .then((data) => {
+                        setLoading(false);
+                        form.reset();
+                        console.log(data);
+                        Swal.fire("Thanks for contacting us. We will get back to you shortly", "", "success");
+                  });
+      };
+      return (
             <div>
-              <p>Name*</p>
-              <input
-                type="text"
-                name="name"
-                required
-                autoComplete="off"
-                className="w-full md:w-[500px] bg-[#78909C] focus:outline-none pt-3 px-1 border-b-[1px] border-[#FFFFFF40] text-white font-roboto font-light"
-              />
-            </div>
-            <div>
-              <p>Enter your Email</p>
-              <input
-                type="text"
-                name="email"
-                autoComplete="off"
-                className="w-full md:w-[500px] bg-[#78909C] focus:outline-none pt-3 px-1 border-b-[1px] border-[#FFFFFF40] text-white font-roboto font-light"
-              />
-            </div>
-            <div>
-              <p>Phone Number*</p>
-              <input
-                type="text"
-                name="phone"
-                required
-                autoComplete="off"
-                className="w-full md:w-[500px] bg-[#78909C] focus:outline-none pt-3 px-1 border-b-[1px] border-[#FFFFFF40] text-white font-roboto font-light"
-              />
-            </div>
-            <div>
-              <p>Message</p>
-              <input
-                type="text"
-                name="message"
-                required
-                autoComplete="off"
-                className="w-full md:w-[500px] bg-[#78909C] focus:outline-none pt-10 px-1 border-b-[1px] border-[#FFFFFF40] text-white font-roboto font-light"
-              />
-            </div>
+                  <Helmet>
+                        <title>Contact | SEA Properties Ltd.</title>
+                  </Helmet>
+                  {/* Contact banner section */}
+                  <PrimaryBanner
+                        title={"Get in touch"}
+                        subTitle={"Contact"}
+                        bannerImg={ss}
+                        opacity={30}
+                  ></PrimaryBanner>
 
-            <div className="pt-6">
-              <input
-                type="submit"
-                value={loading ? "Sending" : "Submit"}
-                className="py-[9px] px-[28px] border-[3px] border-white hover:cursor-pointer  hover:bg-[#a20e0e] hover:text-light"
-              />
-            </div>
-          </form>
-        </div>
-      </div>
+                  {/* Contact Info */}
+                  <div className="grid md:grid-cols-2">
+                        <figure>
+                              <img src={con2} className="w-full h-[390px] object-cover" />
+                        </figure>
+                        <div className="space-y-6 pl-6 md:pl-[50px] lg:pl-[100px] my-auto py-6 md:py-0">
+                              <SecondaryTitle text={title} position="text-left" />
+                              <p className="text-[24px]">{companyName}</p>
 
-      {/* location */}
-      <div className="w-full h-[550px] lg:h-[600px] pb-20">
-        <ContactMap />
-      </div>
-    </div>
-  );
+                              <div>
+                                    <h1 className="md:text-[24px] text-[20px]"> Our Address:</h1>
+
+                                    <p className="md:text-[24px] md:block hidden text-[20px]" >
+                                          100 North Kalshi,
+                                          Gate No - 01,
+                                          Mirpur DOHS <br /> Dhaka, Bangladesh
+                                    </p>
+
+                                    <p className="md:text-[24px] md:hidden block text-[20px]" >
+                                          100 North Kalshi,
+                                          Gate No - 01,
+                                          Mirpur DOHS,  Dhaka, Bangladesh
+                                    </p>
+
+                              </div>
+
+                              <div>
+                                    {phone?.map((item, index) => (
+                                          <p className="md:text-[24px] text-[20px]" key={index}>{item}</p>
+                                    ))}
+                              </div>
+                        </div>
+                  </div>
+
+                  {/* Contact form */}
+                  <div className="bg-[#768f9b] py-[80px] lg:py-[105px]">
+                        <div className="max-w-[1366px] mx-auto px-6 md:px-[90px] lg:px-[260px] text-white">
+                              <h2 className="text-[27px] md:text-[37px] uppercase">
+                                    Book A Free Appointment
+                              </h2>
+                              <form onSubmit={handleSubmit} className="space-y-4 mt-10">
+                                    <div>
+                                          <p>Name*</p>
+                                          <input
+                                                type="text"
+                                                name="name"
+                                                required
+                                                autoComplete="off"
+                                                className="w-full md:w-[500px] bg-[#78909C] focus:outline-none pt-3 px-1 border-b-[1px] border-[#FFFFFF40] text-white font-roboto font-light"
+                                          />
+                                    </div>
+                                    <div>
+                                          <p>Enter your Email</p>
+                                          <input
+                                                type="text"
+                                                name="email"
+                                                autoComplete="off"
+                                                className="w-full md:w-[500px] bg-[#78909C] focus:outline-none pt-3 px-1 border-b-[1px] border-[#FFFFFF40] text-white font-roboto font-light"
+                                          />
+                                    </div>
+                                    <div>
+                                          <p>Phone Number*</p>
+                                          <input
+                                                type="text"
+                                                name="phone"
+                                                required
+                                                autoComplete="off"
+                                                className="w-full md:w-[500px] bg-[#78909C] focus:outline-none pt-3 px-1 border-b-[1px] border-[#FFFFFF40] text-white font-roboto font-light"
+                                          />
+                                    </div>
+                                    <div>
+                                          <p>Message</p>
+                                          <input
+                                                type="text"
+                                                name="message"
+                                                required
+                                                autoComplete="off"
+                                                className="w-full md:w-[500px] bg-[#78909C] focus:outline-none pt-10 px-1 border-b-[1px] border-[#FFFFFF40] text-white font-roboto font-light"
+                                          />
+                                    </div>
+
+                                    <div className="pt-6">
+                                          <input
+                                                type="submit"
+                                                value={loading ? "Sending" : "Submit"}
+                                                className="py-[9px] px-[28px] border-[3px] border-white hover:cursor-pointer  hover:bg-[#a20e0e] hover:text-light"
+                                          />
+                                    </div>
+                              </form>
+                        </div>
+                  </div>
+
+                  {/* location */}
+                  <div className="w-full h-[550px] lg:h-[600px] pb-20">
+                        <ContactMap />
+                  </div>
+            </div>
+      );
 };
 
 export default Contact;
--- src/pages/Home/News_Event/NewsEventReadMore.jsx
+++ src/pages/Home/News_Event/NewsEventReadMore.jsx
@@ -9,60 +9,66 @@ import Slider from "../../../components/common/Slider";
 const NewsEventReadMore = () => {
 
 
-    const [selectedImageIndex, setSelectedImageIndex] = useState(null);
+      const [selectedImageIndex, setSelectedImageIndex] = useState(null);
 
-    const news_events = useLoaderData()
-    console.log(news_events);
-    const { galleryImg, featureImg, title, description } = news_events
-
-
-    const openSlider = (index) => {
-        setSelectedImageIndex(index);
-    };
-
-    const closeSlider = () => {
-        setSelectedImageIndex(null);
-    };
-
-    return (
-        <div>
-            {/* News and events read more banner section */}
-            <PrimaryBanner
-                title={title}
-                subTitle={'News & Events'}
-                bannerImg={featureImg}
-                opacity={30}
-            ></PrimaryBanner>
-
-            <div className=" pb-16 md:pb-[120px] px-4 md:px-0 max-w-[1366px] mx-auto">
-                <div dangerouslySetInnerHTML={{ __html: description }} className=" mx-auto py-[60px] space-y-6"></div>
+      const news_events = useLoaderData()
+      const { galleryImg, featureImg, title, description, date } = news_events
 
 
+      const openSlider = (index) => {
+            setSelectedImageIndex(index);
+      };
+
+      const closeSlider = () => {
+            setSelectedImageIndex(null);
+      };
+
+
+      return (
+            <div>
+                  <div className="my-[120px] px-10 max-w-7xl mx-auto ">
+                        <section class="">
+                              <div class=" px-4 md:px-0  mx-auto">
+                                    <div class="">
+                                          <div class="">
+                                                <h1 class="text-4xl font-bold text-gray-900 sm:text-5xl">7 Best Growth Hacking Tips for Startups & SaaS</h1>
+                                                <p class="mt-6 text-base font-medium text-gray-500">{new Date(date).toDateString()} </p>
+                                          </div>
+                                    </div>
+                              </div>
+                        </section>
+
+                        <div className="  px-4 md:px-0 mx-auto">
+                              <div dangerouslySetInnerHTML={{ __html: description }} className=" mx-auto pt-[60px] space-y-6"></div>
+
+
+                        </div>
+
+                        {/* news and event gallery section  */}
+                        <div className=" grid grid-cols-2 md:grid-cols-3">
+                              {
+                                    galleryImg?.map((img, index) => <img
+                                          key={index}
+                                          src={img}
+                                          onClick={() => openSlider(index)}
+                                          className="w-full h-[170px] md:h-[215px] lg:w-[415px] mt-4 border rounded lg:h-[415px] object-cover hover:cursor-pointer hover:contrast-125 transition-all duration-500" />)
+                              }
+                        </div>
+
+                        {/* gallery images slider view */}
+
+                  </div>
+                  {selectedImageIndex !== null && (
+                        <Slider
+                              images={galleryImg}
+                              selectedIndex={selectedImageIndex}
+                              setSelectedImageIndex={setSelectedImageIndex}
+                              onClose={closeSlider}
+                        />
+                  )}
             </div>
 
-            {/* news and event gallery section  */}
-            <div className="max-w-[1366px] gap-4 mx-auto md:px-5 xl:px-[60px] grid grid-cols-2 md:grid-cols-3">
-                {
-                    galleryImg?.map((img, index) => <img
-                        key={index}
-                        src={img}
-                        onClick={() => openSlider(index)}
-                        className="w-full h-[170px] md:h-[215px] lg:w-[415px] mt-4 border rounded lg:h-[415px] object-cover hover:cursor-pointer hover:contrast-125 transition-all duration-500" />)
-                }
-            </div>
-
-            {/* gallery images slider view */}
-            {selectedImageIndex !== null && (
-                <Slider
-                    images={galleryImg}
-                    selectedIndex={selectedImageIndex}
-                    setSelectedImageIndex={setSelectedImageIndex}
-                    onClose={closeSlider}
-                />
-            )}
-        </div>
-
-    );
+      );
 };
 
 export default NewsEventReadMore;
